2d8f820524e9a3c06282e7aec9497e8ee72014bc
Ruslan Sosnov <218.run@gmail.com>
2021-06-19 22:21:18 +0300

    Header was added

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,134 @@
-const { task } = require("gulp");
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const pug = require("gulp-pug");
-const sass = require("gulp-sass");
-const spritesmith = require("gulp.spritesmith");
-const rimraf = require("rimraf");
-const rename = require("gulp-rename");
+let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
-/* --------- Server ---------*/
-gulp.task("server", function () {
-  browserSync.init({
-    server: {
-      port: 9000,
-      baseDir: "build",
-    },
-  });
+const { src, dest, parallel, series, watch } = require('gulp')
+const browserSync  = require('browser-sync').create()
+const bssi         = require('browsersync-ssi')
+const ssi          = require('ssi')
+const webpack      = require('webpack-stream')
+const sass         = require('gulp-sass')
+const sassglob     = require('gulp-sass-glob')
+const less         = require('gulp-less')
+const lessglob     = require('gulp-less-glob')
+const styl         = require('gulp-stylus')
+const stylglob     = require("gulp-noop")
+const cleancss     = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename       = require('gulp-rename')
+const imagemin     = require('gulp-imagemin')
+const newer        = require('gulp-newer')
+const rsync        = require('gulp-rsync')
+const del          = require('del')
+const pug          = require('gulp-pug')
 
-  gulp.watch("build/**/*.*").on("change", browserSync.reload);
-});
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+	})
+}
 
-/* --------- Pug compile ---------*/
-gulp.task("templates:compile", function buildHTML() {
-  return gulp
-    .src("source/template/index.pug")
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(gulp.dest("build"));
-});
+function scripts() {
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+		.pipe(webpack({
+			mode: 'production',
+			performance: { hints: false },
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env'],
+							plugins: ['babel-plugin-root-import']
+						}
+					}
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(rename('app.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
 
-/* --------- Style compile ---------*/
-gulp.task("styles:compile", function buildHTML() {
-  return gulp
-    .src("source/styles/main.scss")
-    .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest("build/css"));
-});
+function templates() {
+  return src([`app/template/*.*`, `!app/template/_*.*`])
+    .pipe(pug({pretty: true}))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
 
-/* --------- Images compile ---------*/
-gulp.task("sprite", function (cb) {
-  const spriteData = gulp.src("source/images/icons/*.png").pipe(
-    spritesmith({
-      imgName: "sprite.png",
-      imgPath: "../images/sprite.png",
-      cssName: "sprite.scss",
-    })
-  );
-  spriteData.img.pipe(gulp.dest("build/images/"));
-  spriteData.css.pipe(gulp.dest("build/styles/global/"));
-  cb();
-});
+function styles() {
+	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
+		.pipe(eval(`${preprocessor}glob`)())
+		.pipe(eval(preprocessor)())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
 
-/* --------- Delete ---------*/
-gulp.task("clean", function del(cb) {
-  return rimraf("build", cb);
-});
+function images() {
+	return src(['app/images/src/**/*'])
+		.pipe(newer('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
+}
 
-/* --------- Copy fonts ---------*/
-gulp.task("copy:fonts", function () {
-  return gulp
-  .src("./source/fonts/**/*.*")
-  .pipe(gulp.dest("build/fonts"));
-});
+function buildcopy() {
+	return src([
+		'{app/js,app/css}/*.min.*',
+		'app/images/**/*.*',
+		'!app/images/src/**/*',
+		'app/fonts/**/*'
+	], { base: 'app/' })
+	.pipe(dest('dist'))
+}
 
-/* --------- Copy images ---------*/
-gulp.task("copy:images", function () {
-  return gulp
-    .src("./source/images/**/*.*")
-    .pipe(gulp.dest("build/images"));
-});
+function cleandist() {
+	return del('dist/**/*', { force: true })
+}
 
-/* --------- Copy ---------*/
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+function deploy() {
+	return src('dist/')
+		.pipe(rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			// clean: true, // Mirror copy with file deletion
+			include: [/* '*.htaccess' */], // Included files to deploy,
+			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}))
+}
 
-/* --------- Watchers ---------*/
-gulp.task('watch', function() {
-    gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
-    gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
-});
+function startwatch() {
+	watch(`app/template/**/*.pug`, { usePolling: true }, templates)
+	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
 
-/* --------- Default ---------*/
-gulp.task('default', gulp.series(
-    'clean',
-    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
-    gulp.parallel('watch', 'server')
-));
+exports.templates = templates
+exports.scripts = scripts
+exports.styles  = styles
+exports.images  = images
+exports.deploy  = deploy
+exports.assets  = series(templates, scripts, styles, images)
+exports.build   = series(cleandist, templates, scripts, styles, images, buildcopy)
+exports.default = series(templates, scripts, styles, images, parallel(browsersync, startwatch))
